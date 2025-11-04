2c0f594f0fa0a36763db03f5e90368e3aa63bac9
JoshuaOsasDev <ebighoselejoshua@gmail.com>
2025-11-04 18:43:54 +0000

    added redirect page

--- app/_libs/auth.ts
+++ app/_libs/auth.ts
@@ -2,11 +2,13 @@
 import { signIn, signOut } from "@/auth";
 
 export const login = async function (provider: "github" | "google") {
-  await signIn(provider, { redirectTo: "/" });
+  await signIn(provider, {
+    redirectTo: "https://job-search-five-nu.vercel.app/",
+  });
 };
 
 export const logout = async function () {
-  await signOut({ redirectTo: "/auth/signin" });
+  await signOut({ redirectTo: "https://job-search-five-nu.vercel.app/" });
 };
 
 // export async function signInAction(provider: "github") {
